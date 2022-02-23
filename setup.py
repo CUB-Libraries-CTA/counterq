0974f876b862555a992bdee6208aff06ad9f9472
Mark Stacy <mbstacy@gmail.com>
2022-02-23 09:59:57 -0700

    add dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(name='counterq',
       version='0.0',
       packages= find_packages(),
-      install_requires=['sqlalchemy==1.4.31',],
+      install_requires=[
+            'sqlalchemy==1.4.31',
+            'pandas',
+            'boto3',
+            'XlsxWriter==3.0.1',
+            ],
 )
