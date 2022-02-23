a5cfddc73619e16967849cf845c930d66644a192
Mark Stacy <mbstacy@gmail.com>
2022-02-23 09:35:22 -0700

    add dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name='counterq',
       version='0.0',
       packages= find_packages(),
-      install_requires=[],
+      install_requires=['sqlalchemy==1.4.31',],
 )
